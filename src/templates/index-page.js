359a26d5f0548c4eded8f8736d8be65315598f1f
luida-yamazaki <yamazaki@luida.jp>
2019-09-25 16:40:24 +0900

    スマホでpositionfixed効かないので削除。

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,8 +22,7 @@ export const IndexPageTemplate = ({
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top center`,
-        backgroundAttachment: `fixed`,
+        backgroundPosition: `top center`
       }}
     >
       <div
